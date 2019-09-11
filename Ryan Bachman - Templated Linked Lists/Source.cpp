30bb7de4d88dc46721fa8fec28739b2846bc80ed
RYAN BACHMAN <RYABACH1@uat.edu>
2019-09-11 10:46:20 -0700

    Updated notes necessary for project completion.

--- Ryan Bachman - Templated Linked Lists/Source.cpp
+++ Ryan Bachman - Templated Linked Lists/Source.cpp
@@ -8,14 +8,17 @@
 
 		Find(dataToFind) – returns a pointer to the node whose key matches dataToFind.
 
-		Insert(newData) – inserts a new node at the end of the list with a key equal to newData
+		Insert(newData) – inserts a new node at the end of the list with a key equal to newData.
 
 		Delete(delNode)- deletes the node that delNode points to from the list.
 
 		For extra credit, implement a function that finds the Minimum and Maximum values in your linked list.
 
-			Minimum() – returns the minimum key in the list
+			Minimum() – returns the minimum key in the list.
 
-			Maximum() –returns the maximum key in the list
+			Maximum() – returns the maximum key in the list.
 
  Your main program should instantiate, initialize and correctly demonstrate the functionality of the linked list.*/
+
+// Templated add functions will do the function no matter what the data type.
+// Will need current and previous pointers so we can move backwards if need be. Previous will always be one node behind.
